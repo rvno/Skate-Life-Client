2975497592db69e5edec6cd5414eb3679a397273
Harvey Ngo <harveydngo@gmail.com>
2015-08-09 17:02:16 -0700

    add google map div to main map page, check to see if it works on phonegap build

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ $(document).on("click", ".skatepark-link", function(e){
 			response.name = response.name + " skatepark"
 		}
 
-		$('#skatepark-page .skatepark-name').text(response.name)
+		$('#skatepark-page .skatepark-name').text(response.name.toUpperCase());
 		$('#skatepark-page .ui-content .skatepark-page').html('<h1>'+response.name+'</h1><p>Address: '+response.address+'</p><p>Favorited: '+response.fav_count+'</p>'
 			)
 
@@ -61,8 +61,50 @@ $(document).on("click", ".skatepark-link", function(e){
 	// window.location.replace('#skatepark-page')
 })
 
-$(document).ready(function(){
-	$('.login-btn').on('click', function(e){
-		console.log("something funny")
-	})
-})
+// $(document).ready(function(){
+// 	$('.login-btn').on('click', function(e){
+// 		console.log("something funny")
+// 	})
+// })
+
+
+// BEGIN BUILDING MAP
+var dbc = new google.maps.LatLng(37.784595, -122.397224)
+
+function initializeMap(){
+	var mapProp = {
+		center:dbc,
+		zoom:17,
+		mapTypeId:google.maps.MapTypeId.HYBRID,
+    panControl:false,
+    zoomControl:true,
+    zoomControlOptions: {
+      style:google.maps.ZoomControlStyle.SMALL,
+      // position:google.maps.ControlPosition.BOTTOM_RIGHT
+    },
+    mapTypeControl:false,
+    scaleControl:false,
+    streetViewControl:true,
+    overviewMapControl:false,
+    rotateControl:false,
+  };
+
+  // actually Build the map
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  map.setTilt(0); //tilt is the angle at which you view the map (think bird's eye)
+  // Grab radius of skatepark
+  // var skateparkRadius = new google.maps.Circle({
+  //   center:portrero,
+  //   radius:50,
+  //   strokeColor:"#0000FF",
+  //   strokeOpacity:1,
+  //   strokeWeight:2,
+  //   fillColor:"#0000FF",
+  //   fillOpacity:0.4
+  // });
+	//skateparkRadius.setMap(map);
+	// THE SECTION ABOVE REPRESENTS THE BLUE CIRCLE FOR GEOFENCING, NOT MVP
+
+}
+
+google.maps.event.addDomListener(window, 'load', initializeMap);
