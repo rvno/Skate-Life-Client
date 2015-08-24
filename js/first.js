13b768b5e7c542f580d2c87df250578fb0d3c00b
Chris Scott <christo247@gmail.com>
2015-08-24 11:39:46 -0700

    Add first.js at top of custom scripts, gets current location

---
+++ js/first.js
@@ -0,0 +1,21 @@
+var currentLocation;
+
+
+var setCurrentUserPosition = function(position) {
+  currentLocation = new google.maps.LatLng(
+    position.coords.latitude,
+    position.coords.longitude
+  );
+  debugger
+}
+
+var onError = function() {
+  alert('code: '    + error.code    + '\n' + 
+        'message: ' + error.message + '\n');
+}
+
+var getCurrentLocation = function() {
+  navigator.geolocation.getCurrentPosition(setCurrentUserPosition, onError);
+}
+
+getCurrentLocation();
