7076904d01b3a1313fad4bccdad77f9f31963336
Chris Scott <christo247@gmail.com>
2015-08-25 08:26:01 -0700

    Remove baseURL from users.js

--- js/users.js
+++ js/users.js
@@ -1,6 +1,3 @@
-var baseURL = 'https://skate-life-backend.herokuapp.com/';
-// var currentUserId;
-
 // MOVE THIS TO APP.JS, or LAYOUT.JS?
 var externalPanel = '<div data-role="panel" id="favoritesPanel" data-display="overlay" data-theme="b"><a href="#" data-rel="close" class="ui-btn ui-btn-inline ui-shadow ui-corner-all ui-btn-a ui-icon-delete ui-btn-icon-left" data-prefetch >Close Favorites</a><ul data-role="listview" class="favorites"><li id="logout"><a href="#">Logout</a></li></ul></div>';
 var chatPanel = '<div data-role="panel" id="chatPanel" data-display="overlay" data-position="right" data-theme="b"><a href="#" data-rel="close" class="ui-btn ui-btn-inline ui-shadow ui-corner-all ui-btn-a ui-icon-delete ui-btn-icon-right" data-prefetch >Close Messages</a><ul data-role="listview" class="chat-messages"></ul></div>';
