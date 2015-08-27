1843fb5b0a6c5f00b31a3eda775a6a387f1732e7
Chris Scott <christo247@gmail.com>
2015-08-27 12:03:10 -0700

    Change name of first.js to location_logic.js

---
+++ js/location_logic.js
@@ -0,0 +1,67 @@
+var currentLocation;
+
+
+
+var getCurrentLocation = function() {
+  navigator.geolocation.getCurrentPosition(setCurrentUserPosition, onError);
+}
+
+var setCurrentUserPosition = function(position) {
+  currentLocation = new google.maps.LatLng(
+    position.coords.latitude,
+    position.coords.longitude
+  );
+}
+
+var onError = function() {
+  alert('code: '    + error.code    + '\n' + 
+        'message: ' + error.message + '\n');
+}
+
+
+
+
+
+var addLocationButtons = function() {
+  var homeControlDiv = document.createElement('div');
+  var homeControl = new LocationButton(homeControlDiv, map, 'Home');
+
+  var currentLocationCtrlDiv = document.createElement('div');
+  var currentLocationControl = new LocationButton(currentLocationCtrlDiv, map, 'My Location');
+  
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(currentLocationCtrlDiv);
+}
+
+
+function LocationButton(controlDiv, map, buttonText) {
+  controlDiv.style.padding = '5px';
+  
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = 'orange';
+  controlUI.style.color = 'black';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'location button';
+  controlUI.class = "location-btn";
+  controlDiv.appendChild(controlUI);
+
+  var controlText = document.createElement('div');
+  controlText.style.fontFamily = 'Arial', 'sans-serif';
+  controlText.style.fontSize='12px';
+  controlText.style.paddingLeft = '4px';
+  controlText.style.paddingRight ='4px';
+  controlText.innerHTML = '<p>'+buttonText+'</p>';
+  controlUI.appendChild(controlText);
+
+
+  google.maps.event.addDomListener(controlUI, 'click', function () {
+    if (buttonText === 'Home') {
+      map.panTo(currentUser.marker.position);
+    } else {
+      map.panTo(currentLocation);
+      currentUser.marker.setPosition(currentLocation);
+    }
+  });
+
+}
