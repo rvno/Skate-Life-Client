b079d7c03d3629dee69ccfa91c9b49eb5dd7f196
Harvey Ngo <harveydngo@gmail.com>
2015-09-12 16:07:28 -0700

    Reimplement functionality that loads info window corresponding to the active carousel.
    When the carousel is swiped, the map opens the info window for the skatepark that has the same name as the active skatepark element within the carousel.

--- js/carousel.js
+++ js/carousel.js
@@ -8,15 +8,46 @@ $('.carousel').slick({
 
 //upon moving the marker position, check the boundaries of the marker against the position of the markers
 //if the marker is within that position, we populate the carousel with the names of those skateparks
+//
 
 
 var buildCarouselElement = function(skatepark){
 	//
 	var parkCarouselElement = 
-		$('<div>').addClass('carousel-element').append(
-			$('<p>').text(skatepark.name).attr('id', skatepark.id)
+		$('<div>').addClass('carousel-element').attr('id', skatepark.id).append(
+			$('<p>').text(skatepark.name)
 			) 
 
 	skatepark.carouselElement = parkCarouselElement
 
 }
+
+$('.carousel').on('afterChange', function(){
+	var parkId = $('.slick-active').attr('id')
+	allSkateparks.forEach(function(park){
+		park.infoWindow.close(map)
+		if(park.id === parseInt(parkId)){
+			park.infoWindow.open(map);
+		}
+	})	
+})
+
+// $('.carousel').on('afterChange', function(){
+//   imageSrc = $('.slick-active > img').attr('src')
+//   parkLocation = grabLocationFromURL(imageSrc)
+//   console.log(parkLocation)
+//   newMarkerLat = parkLocation.lat
+//   newMarkerLong = parkLocation.lon
+//   console.log(newMarkerLat)
+//   console.log(newMarkerLong)
+//   newCenterPoint = {lat: newMarkerLat,lng: newMarkerLong}
+//   map.panTo(newCenterPoint)
+//   //OPEN INFO WINDOW UPON CAROUSEL SCROLL, time permitting, try to close window when scrolling
+//   $.each(infoWindows, function(index, infoWindow){
+//     if(infoWindow.position['G'] === newCenterPoint['lat'] && infoWindow.position['K'] === newCenterPoint['lng']){
+//       console.log('gotemmmm')
+//       previousWindow = infoWindow;
+//       infoWindow.open(map);
+//     } 
+//   })
+// })
